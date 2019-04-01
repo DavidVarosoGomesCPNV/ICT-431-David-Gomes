866af896b8768cb2d5c10f6ebf9cc09b47333061
DavidVarosoGomesCPNV <48351164+DavidVarosoGomesCPNV@users.noreply.github.com>
2019-04-01 18:51:10 +0200

    Code_Bataille_Navale

---
+++ main.c
@@ -0,0 +1,90 @@
+// Auteur : David Gomes
+// Date : 20.02.2019
+// Version : 1.0
+// But : Créer une jeu Bataille Navale
+
+
+#include <stdio.h>
+#include <windows.h>
+#include "Tableau.h"
+#pragma execution_character_set ("utf-8")
+
+void menu(){
+
+    unsigned int choix = 0;
+
+    while (choix != 1 && choix != 4) //Boucle pour afficher le menu tant que l'utilisateur n'a pas appuiyer sur jouer ou quitter
+    {
+        // Choix de l'utilisateur
+        printf("1 - Jouer\n2 - Option\n3 - Règles du jeu\n4 - Quitter\n\nQuel est votre choix ?\n");
+        scanf("%d", &choix);
+
+        // Menu de base
+        // Les action qui suiveront le choix de l'utilisateur
+
+
+        switch (choix) {
+
+            {
+                case 1:
+                    printf("-----------------------------\n       Bataille navale\n-----------------------------\n\n\n");
+                    printf("Le plateu de jeu ce présente comme ceci : \n\n");
+                    printf("     1    2    3    4    5    6    7    8    9\n"
+                       "1    -    -    -    -    -    -    -    -    -\n"
+                       "2    -    -    -    -    -    -    -    -    -\n"
+                       "3    -    -    -    -    -    -    -    -    -\n"
+                       "4    -    -    -    -    -    -    -    -    -\n"
+                       "5    -    -    -    -    -    -    -    -    -\n"
+                       "6    -    -    -    -    -    -    -    -    -\n"
+                       "7    -    -    -    -    -    -    -    -    -\n"
+                       "8    -    -    -    -    -    -    -    -    -\n"
+                       "9    -    -    -    -    -    -    -    -    -");
+
+                    affichageGrille();
+
+
+                break;
+            }
+            {
+                case 2:
+                    printf("En devlopement ...\n\n");
+                    break;
+            }
+            {
+                case 3:
+                    printf("Voici les règles:\n\nIl s'aggit d'un jeu de bataille navale, vous devez couler tout les bataux de votre adversaire aussi vite que possible ou vous risquez d'avoir les poissons comme voisins !\n\n");
+                    printf("Il a differents types de bateaux plus ou moins grands : \n => Les torpieurs ( 2 cases ) \n => Les sous-marins ( 3 cases ) \n => Les contre-torpieurs ( 3 cases ) \n => Les croiseurs ( 4 cases ) \n => Les porte-avions ( 5 cases ) \n\n");
+                    printf("Il n'y qu'un bateau de chaque type !!\n\n");
+                break;
+            }
+            {
+                default:
+                    printf("Merci d'avoir joué a ce jeu ! \n");
+                break;
+            }
+        }
+    }
+}
+
+
+
+
+char playername[15];
+
+int main() {
+    SetConsoleOutputCP(65001); //Cette liste permet d'afficher des accents
+
+    printf("Bienvenu dans la bataille navalle !\n");
+    printf("Comment vous appelez-vous capitaine ?\n");
+    scanf("%s", playername);
+    printf("Bienvenu %s !\n", playername);
+
+    printf("Que vouleuz-vous faire ?\n");
+    printf("________________________\n");
+    menu();
+
+
+
+
+return 0;
+}
